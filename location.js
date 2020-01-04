16702cb04118bef730df592532a0e0e1259a8f95
Austen Haymond <ahaymon17@gmail.com>
2020-01-04 11:04:37 -0700

    added html and js files to create geolocation app with gps and geolocation APIs

---
+++ location.js
@@ -0,0 +1,22 @@
+const locations = ["My Location"];
+        (function () {
+            navigator.geolocation.getCurrentPosition(function (position) {
+       console.log(position.coords.latitude)
+       console.log(position.coords.longitude)
+       
+        const queryUrl = "GET https://us1.locationiq.com/v1/reverse.php?key=7f4e3e6f4c7876&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&format=json";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+        
+
+    
+    },
+    function (error) {
+        console.log("The Locator was denied. :(")
+    });
+    })();
