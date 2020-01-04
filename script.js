93069d48b0f1addcdf02f16840844985ff428abc
David Foster <foster_db@yahoo.com>
2020-01-04 12:53:54 -0700

    created script.js added scroll function to buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+function mapScroll() {
+  var mapBtn = document.getElementById("map");
+  mapBtn.scrollIntoView();
+}
+
+function factScroll() {
+  var factBtn = document.getElementById("facts");
+  factBtn.scrollIntoView();
+}
+
+function jokeScroll() {
+  var jokeBtn = document.getElementById("joke");
+  jokeBtn.scrollIntoView();
+}
